8005fd3274d0871674c4ac4ee2783a8c84d0e47c
priyanka <priyanka1461998@gmail.com>
2018-05-31 14:09:33 +0530

    age.java

---
+++ src/com/basic/Age2.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Age2 {
+	public static void main(String[] args)
+	{
+		int x=10,total=30;
+	while ( x <= 100 )
+	{
+		total += x;
+		++x;
+System.out.println(total);
+	}
+}
+}
