663223d6e652dfd6451c88c652a02f8ca350cf06
priyanka <priyanka1461998@gmail.com>
2018-05-31 14:52:48 +0530

    program10

---
+++ src/com/basic/Priya.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Priya {
+	public static void main(String[] args)
+	{
+	  int x = 1, total=20;
+	 while ( x <= 10 )
+	 {
+	 total += x;
+	 ++x;
+	 System.out.println(total);
+	 }
+	}
+}
