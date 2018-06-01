b453578f7a181ef826327391dbeec008f044a86c
priyanka <priyanka1461998@gmail.com>
2018-06-01 17:02:48 +0530

    date.java

--- src/com/basic/Date.java
+++ src/com/basic/Date.java
@@ -1,49 +1,44 @@
 package com.basic;
 
 public class Date {
-	int day;
-	int year;
-	int month;
-	public Date() {
-		
-	}
-	public Date(int month,int day,int year)
-	{
-		this .month=month;
-		this.day=day;
-		this .year=year;
-		}
-	public int getDay() {
-		return day;
-	}
-	public void setDay(int day) {
-		this.day = day;
-	}
-	public int getYear() {
-		return year;
-	}
-	public void setYear(int year) {
-		this.year = year;
-	}
-	public int getMonth() {
-		return month;
-	}
-	public void setMonth(int month) {
-		this.month = month;
-	}
-public void display()
-{
-System.out.println(date +  " ." +month+ " ." +year +".");	
-}
-public static void main(String[] args)
-{
-Date d1=new Date();
-Month m1=new Month();
-Year y1=new Year();
-
-	p1.setdate(31);
-	p2.setmonth(5);
-	p3.setyear(2018);
+int day;
+int month;
+int year;
+public Date(int day,int month,int year) {
+	this.day=day;
+	this.month=month;
+	this.year=year;
 	
+}
+public int getDay() {
+	return day;
+}
+public void setDay(int day) {
+	this.day = day;
+}
+public int getMonth() {
+	return month;
+}
+public void setMonth(int month) {
+	this.month = month;
+}
+public int getYear() {
+	return year;
+}
+public void setYear(int year) {
+	this.year = year;
+}
+	public void displayDate()
+	{
+		System.out.println(day+"/"+month+"/"+year);
+	}
 
+public static void main(String args[])
+{
+	Date d1=new Date(1,12,2015);
+	d1.displayDate();
+	
+}	
+	
+}
 
