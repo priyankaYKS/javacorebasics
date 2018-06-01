a21898ed1f38b235fc580bbf601c4be7bb20e5a3
priyanka <priyanka1461998@gmail.com>
2018-06-01 16:07:25 +0530

    Boolean.java

---
+++ src/com/basic/Boolean.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Boolean {
+	
+	public boolean isevent(isone) {
+		int one;
+	return((one%10)==0? true:false;
+	public static void main(String[] args)
+	{
+		
+	}
+}
+}
+
