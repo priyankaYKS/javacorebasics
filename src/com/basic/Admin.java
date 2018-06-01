8b67bafa41d774e545d7a52a9388a292f5d69ca9
priyanka <priyanka1461998@gmail.com>
2018-06-01 16:06:42 +0530

    array.java

---
+++ src/com/basic/Admin.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+public class Admin {
+	public static void main(String[] args)
+	{
+	int N=78945;
+	int t=N;
+	int arr[]=new int[10];
+	int i=0;
+	while(t>0) {
+		int remainder = t % 10;
+		arr[i] =remainder;
+		i++;
+		t=t/10;
+	}
+		for(i=0;i<10;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+
+}
