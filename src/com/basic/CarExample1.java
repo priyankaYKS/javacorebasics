a067a598840dc6e849956d9967350efa2aaac916
priyanka <priyanka1461998@gmail.com>
2018-06-07 10:53:36 +0530

    video example problem

---
+++ src/com/basic/CarExample1.java
@@ -0,0 +1,26 @@
+package com.basic;
+
+public class CarExample1 {
+	int speed;
+	String model;
+	String type;
+	public  CarExample1() {
+		System.out.println("empty cons");
+	}
+	public  CarExample1(int speed) {
+		this.speed=speed;
+		System.out.println("one args cons");
+	}
+	public CarExample1(String model,String type) {
+		this.model=model;
+		this.type=type;
+		System.out.println("two args cons");
+	}
+	public static void main(String[] args) {
+		CarExample1 carExampleOne=new 	CarExample1(100);
+		CarExample1 carExampleTwo=new 	CarExample1("Audi","pep");
+		
+	}
+	
+
+}
