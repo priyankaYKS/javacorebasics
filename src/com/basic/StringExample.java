2aac9af87a3e9116f49a580ee11534f5cfd47626
priyanka <priyanka1461998@gmail.com>
2018-06-13 11:56:16 +0530

    29

---
+++ src/com/basic/StringExample.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class StringExample {
+	public StringExample() {
+		
+	}
+	public static void main(String[] args) {
+		
+		String One="Good";
+		 String stringObject=new  String("Good");
+		 String two="Good";
+		 System.out.println(One==two);
+		 System.out.println(One.equals (two));
+				
+		
+		
+	}
+	
+	
+
+}
