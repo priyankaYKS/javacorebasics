d80d729b49b4933b21670d96987835606b11eabb
priyanka <priyanka1461998@gmail.com>
2018-06-17 12:12:44 +0530

    example

---
+++ src/com/basic/AreaOfCircle.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class AreaOfCircle {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/com/basic/CollectionEx.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionEx {
+	public static void main(String[] args) {
+		
+		List<String> arrayList=new ArrayList<>();
+		arrayList.add("one");
+		arrayList.add("two");
+		arrayList.add("three");
+		arrayList.add("one");
+		System.out.println(arrayList.toString());
+		//LinkedList Example(Node)
+		List<String> linkedList=new LinkedList<>();
+		linkedList.add("one");
+	
+	
+	
+	}
+
+}
