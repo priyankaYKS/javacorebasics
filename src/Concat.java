25e920b376821ebf06ef1f2eb36b807630c50a2f
priyanka <priyanka1461998@gmail.com>
2018-06-26 10:47:05 +0530

    Concat StringMethod

---
+++ src/Concat.java
@@ -0,0 +1,12 @@
+
+public class Concat {
+	public static void main(String[] args) {
+		String str1="priya";
+		
+	
+	String str2="this";
+	System.out.println(str2.concat(" is").concat(" a").concat(" apple"));
+		
+	}
+
+}
