789a1f7664ce7054b206918db3f116dc16715795
priyanka <priyanka1461998@gmail.com>
2018-06-26 10:46:13 +0530

    example program

---
+++ src/ChatAtString.java
@@ -0,0 +1,8 @@
+
+public class ChatAtString {
+public static void main(String[] args) {
+	String str="java python";
+	char ch1=str.charAt(5);
+	System.out.println("charecter at 5 index is" +ch1);
+}
+}
