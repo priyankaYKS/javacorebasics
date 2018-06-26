16c2f79e8d43b8756d4d8e00239a608cde77ae88
priyanka <priyanka1461998@gmail.com>
2018-06-26 10:45:34 +0530

    string

---
+++ src/OneString.java
@@ -0,0 +1,8 @@
+
+public class OneString {
+	public static void main(String[] args) {
+		String str="sneka loves vikram";
+	char ch1=str.charAt(7);
+System.out.println("character at 7 index is "+ch1);
+}
+}
