9a0434e9a799b3486f6b0d44697b84d7965ccd33
priyanka <priyanka1461998@gmail.com>
2018-06-26 11:35:29 +0530

    string

---
+++ src/CampareToString.java
@@ -0,0 +1,13 @@
+
+public class CampareToString {
+	public static void main(String[] args) {
+		  String str1 = "String method tutorial";
+	       String str2 = "compareTo method example";
+	       String str3 = "String method tutorial";
+
+	       int var1 = str1.compareTo( str2 );
+	       System.out.println("str1 & str2 comparison: "+var1);
+		
+	}
+
+}
---
+++ src/SubStringMethod.java
@@ -0,0 +1,10 @@
+
+
+public class SubStringMethod {
+	public static void main(String[] args) {
+		
+		String str1="we are cute and naughty girls";
+		System.out.println(str1.substring(7));
+	}
+
+}
